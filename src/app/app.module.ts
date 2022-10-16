b2b1a99b1fe9f1cabaaf7d8b38b97b2e643d97a5
NimaMeghdadi <meghdadi.nima@gmail.com>
2022-10-16 13:47:00 +0330

    docs(src/app): folder structure
    creating  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddressCardComponent } from './pages/main/components/address-card/address-card.component';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { NotificationComponent } from './shared/components/notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressCardComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
