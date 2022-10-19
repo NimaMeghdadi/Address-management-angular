52764a97c96c5330cadf5840d7c5aab0373b31f2
NimaMeghdadi <meghdadi.nima@gmail.com>
2022-10-19 13:03:06 +0330

    feat(main/components): add address-card

--- src/app/pages/main/components/address-card/address-card.component.ts
+++ src/app/pages/main/components/address-card/address-card.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-address-card',
   templateUrl: './address-card.component.html',
   styleUrls: ['./address-card.component.scss']
 })
 export class AddressCardComponent implements OnInit {
-
+  title:string = 'silam'
   constructor() {
     
    }
-
   ngOnInit(): void {
   }
 
 }
+export class NzDemoCardBorderLessComponent {}
